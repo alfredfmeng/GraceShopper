0e437e2c93462f7bee9f630ffd160311ecbb2906
Alfred Meng <alfredfmeng@gmail.com>
2021-01-12 15:41:09 -0600

    FE: Finished scaffolding AllBooks component

--- client/components/AllBooks.js
+++ client/components/AllBooks.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import SingleBook from './SingleBook'
+import axios from 'axios'
 
 class AllBooks extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  async componentDidMount() {
+    // Placeholder code until backend is set up
+    // try {
+    //   const {data: books} = await axios.get('/api/books')
+    //   this.setState({books})
+    // } catch (err) {
+    //   console.log(err)
+    // }
+  }
+
   render() {
-    return <div />
+    return (
+      <div>
+        {/* {this.state.books.map((book) => {
+          return (
+            <SingleBook
+              key={book.id}
+              title={book.title}
+              author={book.author}
+              description={book.description}
+              genre={book.genre}
+              image={book.image}
+            />
+          )
+        })} */}
+      </div>
+    )
   }
 }
+
+export default AllBooks
