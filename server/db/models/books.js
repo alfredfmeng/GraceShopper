46e7f49438ed8bbe7b5ff1b46c69c0555e169b53
Alfred Meng <alfredfmeng@gmail.com>
2021-01-12 16:44:17 -0600

    BE: updated listenings model to books model

---
+++ server/db/models/books.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Books = db.define('book', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  author: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  genre: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    defaultValue:
+      'https://dynamicmediainstitute.org/wp-content/themes/dynamic-media-institute/imagery/default-thesis-abstract.png'
+  }
+})
+
+module.exports = Books
