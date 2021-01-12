e8a93cdb58cf7585bd9d3d8a1b1aa9bdd4f8817b
James Yon <jamesyon55@gmail.com>
2021-01-12 11:31:03 -0600

    BE:Created Sequelize Model for users

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -18,6 +18,7 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
+    allowNull: false,
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
@@ -26,6 +27,7 @@ const User = db.define('user', {
   },
   salt: {
     type: Sequelize.STRING,
+    allowNull: false,
     // Making `.salt` act like a function hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
