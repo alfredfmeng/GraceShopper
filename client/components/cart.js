f23ddbc7c0f5d817ddd09fa6ecd6a0a18f2f7e2e
eficek <eficek@protonmail.com>
2021-01-13 16:25:50 -0600

    feat: added cart component, express routing

---
+++ client/components/cart.js
@@ -0,0 +1,45 @@
+import React from "react";
+import axios from "axios";
+import SingleBook from "./SingleBook";
+
+export class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      ids: [2, 3],
+      books: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      let res = { ...this.state };
+      for (let i = 0; i < this.state.ids.length; i++) {
+        const response = await axios.get(`/api/books/${this.state.ids[i]}`);
+        res.books.push(response.data);
+      }
+      this.setState(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    // refactor to render table as opposed to divs later
+    return (
+      <div>
+        <h1>Your Cart</h1>
+        {this.state.books.map(book => (
+          <SingleBook
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            description={book.description}
+            genre={book.genre}
+            image={book.image}
+          />
+        ))}
+      </div>
+    );
+  }
+}
