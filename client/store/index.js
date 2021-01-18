160d4bd910b6b519bcbea462edaa237d6e0de994
Alfred Meng <alfredfmeng@gmail.com>
2021-01-18 12:10:33 -0600

    Added newUser reducer to combineReducers

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,12 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import books from './books'
+import newUser from './newUser'
 
 const reducer = combineReducers({
   user,
-  books
+  books,
+  newUser
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
