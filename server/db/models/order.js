e16e897bd7a9e1e79dfd69da1a4820de39a3cb13
eficek <eficek@protonmail.com>
2021-01-19 11:55:18 -0600

    fix: updated book model name, updated foreign key

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,20 +1,16 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize')
+const db = require('../db')
 
-const Order = db.define("order", {
+const Order = db.define('order', {
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  user: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  items: {
-    type: Sequelize.ARRAY(Sequelize.JSON),
+  userID: {
+    type: Sequelize.INTEGER,
     allowNull: false
   }
-});
+})
 
-module.exports = Order;
+module.exports = Order
