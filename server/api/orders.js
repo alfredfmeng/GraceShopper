f2fa9e4e6699f96db590a6eb4d0fb8a733b2b943
eficek <eficek@protonmail.com>
2021-01-19 15:00:45 -0600

    feat: added post route to create order

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,21 +1,38 @@
-const router = require("express").Router();
-const { Order } = require("../db/models");
-module.exports = router;
+const router = require('express').Router()
+const {default: axios} = require('axios')
+const {Order} = require('../db/models')
+module.exports = router
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const orders = await Order.findAll();
-    res.json(orders);
+    const orders = await Order.findAll()
+    res.json(orders)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
-    const order = await Order.findByPk(req.params.id);
-    res.json(order);
+    const order = await Order.findByPk(req.params.id)
+    res.json(order)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
+
+// DUMMY DATA, REPLACE WITH REAL LOCALSTORAGE + LOGIN
+const userID = 1
+
+router.post('/', async (req, res, next) => {
+  try {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    const order = await Order.create({user: userID})
+    for (let i = 0; i < cart.length; i++) {
+      const book = await axios.get(`/books/${cart[i].id}`)
+      order.addBook(book)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
